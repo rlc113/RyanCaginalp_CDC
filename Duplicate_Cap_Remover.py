7c662de96203133a6a4262c54ed3514256faf811
rlc113 <46628160+rlc113@users.noreply.github.com>
2023-11-23 01:03:31 -0500

    Add files via upload

--- Duplicate_Cap_Remover.py
+++ Duplicate_Cap_Remover.py
@@ -4,11 +4,16 @@ destination_file = open('fixed_caps.def', 'w')
 sf_lines = source_file.readlines()
 df_lines = []
 
-for line in sf_lines:
-    print(line)
-    exit()
-    df_lines.append(line)
+unique_coords = {}
 
+for line in sf_lines:
+    if "PHY_EDGE" in line:
+        C = str((line.split(" ")[len(line.split(" ")) - 5], line.split(" ")[len(line.split(" ")) - 4]))
+        if C not in unique_coords.keys():
+            unique_coords[C] = 1
+            df_lines.append(line)
+    else: df_lines.append(line)
+    
 destination_file.writelines(df_lines)
 
 source_file.close()
