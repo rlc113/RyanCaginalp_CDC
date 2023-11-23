67f313425c95452c10deae867e6873fa1d9dfa6e
rlc113 <46628160+rlc113@users.noreply.github.com>
2023-11-23 01:10:17 -0500

    Update Netgen_LVS.tcl

--- LVS/Netgen_LVS.tcl
+++ LVS/Netgen_LVS.tcl
@@ -1,2 +1,2 @@
-lvs /content/RyanCaginalp_CDC/LVS/LayoutExtraction_Without_Decap.spice /content/RyanCaginalp_CDC/LVS/SynthesizedExtraction_Without_DC.spice /content/RyanCaginalp_CDC/setup.tcl
+lvs /content/RyanCaginalp_CDC/LVS/LayoutExtraction_Without_Decap.spice /content/RyanCaginalp_CDC/LVS/SynthesizedExtraction_Without_DC.spice /content/RyanCaginalp_CDC/LVS/setup.tcl
 exit
