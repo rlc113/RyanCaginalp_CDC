e323bf9884796922f5eeed8d6e3a37b6fb5c790b
rlc113 <46628160+rlc113@users.noreply.github.com>
2023-11-23 11:36:25 -0500

    Update Magic_Extraction.tcl

--- NgSpice/Magic_Extraction.tcl
+++ NgSpice/Magic_Extraction.tcl
@@ -1,5 +1,5 @@
 # Read the GDS file
-gds read "/home/ryan/openROADTests/Final.gds"
+gds read "/content/RyanCaginalp_CDC/Final.gds"
 
 # Load the CDC
 load CDC
@@ -13,7 +13,7 @@ ext2spice rthresh 0
 ext2spice cthresh 0
 
 # Extract the CDC to a SPICE file
-ext2spice -o /home/ryan/openROADTests/NgSpice/CDC.spice
+ext2spice -o /content/RyanCaginalp_CDC/NgSpice/CDC.spice
 
 # Terminate
 exit
