c3de2155f2bbcb181f9269389c1640a726f50c23
rlc113 <46628160+rlc113@users.noreply.github.com>
2023-11-23 00:43:53 -0500

    Update Netgen_LVS.tcl

--- LVS/Netgen_LVS.tcl
+++ LVS/Netgen_LVS.tcl
@@ -1,2 +1,2 @@
-lvs /home/ryan/openROADTests/LVS/LayoutExtraction_Without_Decap.spice /home/ryan/openROADTests/LVS/SynthesizedExtraction_Without_DC.spice /home/ryan/openROADTests/LVS/setup.tcl
+lvs /content/RyanCaginalp_CDC/LVS/LayoutExtraction_Without_Decap.spice /content/RyanCaginalp_CDC/LVS/SynthesizedExtraction_Without_DC.spice /content/RyanCaginalp_CDC/setup.tcl
 exit
