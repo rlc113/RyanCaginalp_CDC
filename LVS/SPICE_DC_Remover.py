a739654cd121712aee9882a1c2543dac1583ba7c
rlc113 <46628160+rlc113@users.noreply.github.com>
2023-11-23 01:14:51 -0500

    Update SPICE_DC_Remover.py

--- LVS/SPICE_DC_Remover.py
+++ LVS/SPICE_DC_Remover.py
@@ -15,8 +15,8 @@ for l in sf_lines:
 #Also remove last new line and include libraries and transmission_gate simulation files
 df_lines[len(df_lines) - 1] = df_lines[len(df_lines) - 1].replace('\n', '')
 
-df_lines.insert(1, '.include "/home/ryan/MagicTests/SKY130/git_open_pdks/sky130/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice"\n')
-df_lines.insert(1, '.include "transmission_gate.spice"\n')
+df_lines.insert(1, '.include "/content/conda-env/share/pdk/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice"\n')
+df_lines.insert(1, '.include "/content/RyanCaginalp_CDC/LVS/transmission_gate.spice"\n')
 
 df.writelines(df_lines)
 
