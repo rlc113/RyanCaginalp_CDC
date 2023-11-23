ca340a0dd5e51b0c89182f50840144026897f5bb
rlc113 <46628160+rlc113@users.noreply.github.com>
2023-11-23 00:29:26 -0500

    Update DRC.sh

--- DRC.sh
+++ DRC.sh
@@ -1,1 +1,1 @@
-magic -rcfile /home/ryan/MagicTests/SKY130/git_open_pdks/sky130/sky130A/libs.tech/magic/sky130A.magicrc -noconsole -dnull TCL/magic_commands.tcl
+magic -rcfile /content/conda-env/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc -noconsole -dnull TCL/magic_commands.tcl
